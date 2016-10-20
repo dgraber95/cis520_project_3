1e5727a03c61b7a58bb176dbdaa4f634bc63b873
Nolan Casimir <casimirn@flex.cs.ksu.edu>
2016-10-19 20:33:47 -0500

    Fixed stupid errors

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -1,4 +1,15 @@
+#include <debug.h>
+#include <stddef.h>
+#include <random.h>
+#include <stdio.h>
+#include <string.h>
 #include "page.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+
+void init(void);
+void* create_frame(struct sup_page* page);
+void* free_frame(void* addr);
 
 void init(void)
 {
@@ -7,22 +18,25 @@ void init(void)
   list_init(&file_mappings_table);
 }
 
-void* create_frame(struct* sup_page page)
+void* create_frame(struct sup_page* page)
 {
   void* addr = palloc_get_page(PAL_USER);
-  if (frm != NULL)
+  if (addr != NULL)
   {
-    struct frame frm = malloc(sizeof(struct frame));
-    frame->addr = addr;
-    frame->page = page;
-    list_push_back(&frame_table, frame->elem);
+    struct frame* frm = malloc(sizeof(struct frame));
+    frm->addr = addr;
+    frm->page = page;
+    list_push_back(&frame_table, frm->elem);
   }
+
+return addr;
 }
 
 void* free_frame(void* addr)
 {
-  bool found = FALSE;
-  struct list_elem* e= list_begin(&frame_table)
+  bool found = 0;
+  struct list_elem* e= list_begin(&frame_table);
+
   while (!found && e != list_end(&frame_table))
   {
     struct frame* frm = list_entry(e, struct frame, elem);
@@ -31,7 +45,10 @@ void* free_frame(void* addr)
       list_remove(e);
       free(frm);
       palloc_free_page(addr);
-      found = TRUE;
+      found = 1;
     }
+    e = list_next(e);
   }
+
+return addr;
 }
--- pintos/src/vm/page.h
+++ pintos/src/vm/page.h
@@ -1,4 +1,4 @@
-#include "lib/kernal/list.h"
+#include "lib/kernel/list.h"
 
 struct list sup_page_table;
 struct list frame_table;
@@ -7,7 +7,7 @@ struct list file_mappings_table;
 struct sup_page
 {
 
-  struct list_elem elem;
+  struct list_elem* elem;
 };
 
 struct frame
@@ -16,13 +16,16 @@ struct frame
   void* addr;
   struct sup_page* page;
 
-  struct list_elem elem;
+  struct list_elem* elem;
 };
 
 struct file_mapping
 {
 
-  struct list_elem elem;
+  struct list_elem* elem;
 };
 
 
+void init(void);
+void* create_frame(struct sup_page* page);
+void* free_frame(void* addr);
