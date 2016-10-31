fca45f2177385e28d38107630a1eaea3ad49f67a
Dalton Graber <dgraber95@gmail.com>
2016-10-30 21:07:50 -0500

    Begin swap table

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -1,1 +1,17 @@
 #include "swap.h"
+#include "devices/block.h"
+
+void swap_init(void)
+{ 
+    list_init(&swap_table);
+    swap_block = block_get_role(BLOCK_SWAP);
+}
+
+void swap_read(int swap_index, void* dst_frame)
+{
+  for (int i = 0; i < SECTORS_PER_PAGE; i++)
+  {
+    block_read(swap_block, used_index * SECTORS_PER_PAGE + i,
+    (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+  }
+}
--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -1,9 +1,16 @@
 #include "lib/kernel/list.h"
 
+
+
 struct list swap_table;
+struct block* swap_block;
 
-struct swap
+struct swap_slot
 {
-
-  struct list_elem elem;
+    void * addr;
+    bool free;
+    struct list_elem elem;
 };
+
+int swap_push(void* src_frame);
+void swap_fetch(int swap_index, void* dst_frame);
