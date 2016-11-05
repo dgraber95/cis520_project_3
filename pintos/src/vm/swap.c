50e19669586c6f1663bb9985d6d35e3fa8b3d45b
Dalton Graber <dgraber95@gmail.com>
2016-11-05 10:10:25 -0500

    More swap table work. Maybe good maybe bad.

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -7,11 +7,53 @@ void swap_init(void)
     swap_block = block_get_role(BLOCK_SWAP);
 }
 
-void swap_read(int swap_index, void* dst_frame)
+void swap_fetch(int swap_index, void* dst_frame)
 {
+  struct swap_slot* ss = swap_get_slot(swap_index);
+
+  ASSERT(ss != NULL);
+  ASSERT(ss->free == false);
+
+  ss->free = true;
+
   for (int i = 0; i < SECTORS_PER_PAGE; i++)
   {
-    block_read(swap_block, used_index * SECTORS_PER_PAGE + i,
-    (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+    block_read(swap_block, swap_index * SECTORS_PER_PAGE + i, (void*)((int)dst_frame + i * BLOCK_SECTOR_SIZE));
   }
+}
+
+int swap_push(int swap_index, void* dst_frame)
+{
+
+}
+
+struct swap_slot* swap_get_slot(int slot_index)
+{
+  struct list_elem * e;
+  struct swap_slot* ss;
+
+  for(int i = 0, e = list_begin(&swap_table); e != list_end(&swap_table) && i < slot_index; i++, e = list_next(e))
+
+  if(i == slot_index){
+    return list_entry(e, struct swap_slot, elem);  
+  }
+  return NULL;
+}
+
+int swap_get_free()
+{
+  struct list_elem * e;
+  struct swap_slot* ss;
+  for(int i = 0, e = list_begin(&swap_table); e != list_end(&swap_table); e = list_next(e), i++)
+  {
+    ss = list_entry(e, struct swap_slot, elem);
+    if(ss->free == true)
+    {
+      return ss;
+    }
+  }
+
+  ASSERT(list_size(&swap_table) < SWAP_SIZE/PGSIZE);
+  ss = malloc(sizeof(struct swap_slot));
+  list_push_back()
 }
--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -1,6 +1,9 @@
+#include "devices/block.h"
 #include "lib/kernel/list.h"
+#include "threads/vaddr.h"
 
-
+#define SECTORS_PER_PAGE (PGSIZE/BLOCK_SECTOR_SIZE)
+#define SWAP_SIZE 2000000
 
 struct list swap_table;
 struct block* swap_block;
@@ -14,3 +17,4 @@ struct swap_slot
 
 int swap_push(void* src_frame);
 void swap_fetch(int swap_index, void* dst_frame);
+struct swap_slot* swap_get_slot(int swap_index);
